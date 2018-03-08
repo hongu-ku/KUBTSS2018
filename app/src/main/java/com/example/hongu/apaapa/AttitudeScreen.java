eca04875da098a633ef9a41250784e248fca0504
Yuki Takezawa <courage.t0629@gmail.com>
2018-03-08 18:00:19 +0900

    画面の分割

---
+++ app/src/main/java/com/example/hongu/apaapa/AttitudeScreen.java
@@ -0,0 +1,13 @@
+package com.example.hongu.apaapa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AttitudeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attitude_screen);
+    }
+}
